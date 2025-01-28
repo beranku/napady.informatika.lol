f2e9d4de52c292a29d73c1e1ad3e2a818a11fe81
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2025-01-28 01:19:50 +0100

    Change Content-Type to URL encoded and update submitRating to use URLSearchParams for data submission

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'  // Changed to JSON content type
+    'Content-Type': 'application/x-www-form-urlencoded'  // Changed to simple content type
   }
 });
 
@@ -14,7 +14,13 @@ export const api = {
   getUserInfo: () => axiosInstance.get('/uzivatel'),
   getProjects: () => axiosInstance.get('/napady'),
   submitRating: (data) => {
-    // Post JSON data directly
-    return axiosInstance.put('/hodnoceni', data);
+    // Convert data to URL encoded format
+    const params = new URLSearchParams();
+    params.append('emojis', data.emojis);
+    params.append('projectId', data.projectId);
+    params.append('name', data.name);
+    params.append('userId', data.userId);
+    
+    return axiosInstance.post('/hodnoceni', params);
   }
 };
